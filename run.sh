e8874a004eef35f56d44888d47f1e4571740a85e
Rena <rena.p.oliveira@gmail.com>
2017-10-14 15:06:50 -0300

    -Creating functions

--- run.sh
+++ run.sh
@@ -9,11 +9,13 @@ IP=$(cat $VPN_FILE | grep "remote " | awk '{print $2}')
 trap ctrl_c INT
 
 #Check root permission
-if test " `id -u`" != " 0"
-then
-    echo "permission denied (use sudo)"
-    exit 1
-fi
+check_permission() {
+	if test " `id -u`" != " 0"
+	then
+	    echo "permission denied (use sudo)"
+	    exit 1
+	fi
+}
 
 function ctrl_c() {
 	echo "Flushing iptables and exiting"
@@ -21,26 +23,37 @@ function ctrl_c() {
 	exit 1
 }
 
-iptables --flush
-#Allow connection with the VPN IP
-iptables -A OUTPUT -d $IP -j ACCEPT
-iptables -A INPUT -s $IP -j ACCEPT
-#Allow connection through the tunnel
-iptables -A OUTPUT -o $TUNNEL -j ACCEPT
-iptables -A INPUT -i $TUNNEL -j ACCEPT
-#Block all connection through the main interface
-iptables -A OUTPUT -o $INTERFACE -j DROP
-iptables -A INPUT -i $INTERFACE -j DROP
+set_firewall_rules() {
+	iptables --flush
+	#Allow connection with the VPN IP
+	iptables -A OUTPUT -d $IP -j ACCEPT
+	iptables -A INPUT -s $IP -j ACCEPT
+	#Allow connection through the tunnel
+	iptables -A OUTPUT -o $TUNNEL -j ACCEPT
+	iptables -A INPUT -i $TUNNEL -j ACCEPT
+	#Block all connection through the main interface
+	iptables -A OUTPUT -o $INTERFACE -j DROP
+	iptables -A INPUT -i $INTERFACE -j DROP
+}
+
+reconnect() {
+	#Kill older openvpn processes to avoid creating new tunnels
+	kill `ps -ef | grep $VPN_FILE | grep -v "grep" | awk '{print $2}'` > /dev/null 2>&1
+	#Start openvpn
+	openvpn $VPN_FILE & 
+}
 
 #Check every 20 seconds if VPN goes down, then reconnect it
-while [ true ]
-do
-	if [[ $(nmcli connection) != *$TUNNEL* ]]; then
-		#Kill older openvpn processes to avoid creating new tunnels
-		kill `ps -ef | grep $VPN_FILE | grep -v "grep" | awk '{print $2}'`
-		#Start openvpn
-		openvpn $VPN_FILE &
-	fi
-	sleep 20
-done
+check_connection() {
+	while [ true ]
+	do
+		if [[ $(nmcli connection) != *$TUNNEL* ]]; then
+			reconnect
+		fi
+		sleep 20
+	done
+}
 
+check_permission
+set_firewall_rules
+check_connection
