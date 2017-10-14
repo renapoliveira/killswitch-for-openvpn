8c0cbee2e50c45a5e673004c92544b0e0d478c6c
Rena <rena.p.oliveira@gmail.com>
2017-10-14 15:42:30 -0300

    -Removing config -Getting default interface automatically

--- run.sh
+++ run.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-
-#Import config file
-source config
-
-#Get the VPN IP from the VPN file
-IP=$(cat $VPN_FILE | grep "remote " | awk '{print $2}')
-
 trap ctrl_c INT
+VPN_FILE=$1
 
-#Check root permission
 check_permission() {
 	if test " `id -u`" != " 0"
-	then
-	    echo "permission denied (use sudo)"
-	    exit 1
+		then
+		echo "permission denied (use sudo)"
+		exit 1
 	fi
+}
+
+check_argument() {
+	if [ -z $VPN_FILE ]
+		then
+		echo "First argument must be VPN file(.ovpn)"
+		exit 1
+	fi
+}
+
+config() {	
+	#Get the default network interface
+	echo "Detecting your default network interface..."
+	INTERFACE=`route | grep '^default' | grep -o '[^ ]*$'`
+	echo "Using "$INTERFACE
+
+	TUNNEL=tun0
+	echo "Using interface "$TUNNEL " for VPN, change the script if you need another name."	
+
+	#Get the VPN IP from the VPN file
+	echo "Detecting your VPN server address..."
+	IP=`cat $VPN_FILE | grep "remote " | awk '{print $2}'`
+	echo "Using "$IP
 }
 
 function ctrl_c() {
@@ -24,6 +40,7 @@ function ctrl_c() {
 }
 
 set_firewall_rules() {
+	echo "Setting firewall rules"
 	iptables --flush
 	#Allow connection with the VPN IP
 	iptables -A OUTPUT -d $IP -j ACCEPT
@@ -37,14 +54,16 @@ set_firewall_rules() {
 }
 
 reconnect() {
+	echo "Reconnecting VPN"
 	#Kill older openvpn processes to avoid creating new tunnels
-	kill `ps -ef | grep $VPN_FILE | grep -v "grep" | awk '{print $2}'` > /dev/null 2>&1
+	kill `ps -ef | grep "openvpn $VPN_FILE" | grep -v "grep" | awk '{print $2}'` > /dev/null 2>&1
 	#Start openvpn
 	openvpn $VPN_FILE & 
 }
 
 #Check every 20 seconds if VPN goes down, then reconnect it
 check_connection() {
+	echo "Check VPN connection"
 	while [ true ]
 	do
 		if [[ $(nmcli connection) != *$TUNNEL* ]]; then
@@ -55,5 +74,7 @@ check_connection() {
 }
 
 check_permission
+check_argument
+config
 set_firewall_rules
 check_connection
