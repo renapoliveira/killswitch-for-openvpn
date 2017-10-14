cb830d62d936c8987da8c210fcdd59ca1a5d684c
Rena <rena.p.oliveira@gmail.com>
2017-10-14 14:36:53 -0300

    -Getting VPN IP automatically from ovpn file -Killing older openvpn processes before starting a new one avoiding creating new tunnels

--- config.sh
+++ config.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#IP address pf te VPN server
-IP=
 
-#interface used for the connection (eth0, wlan0, etc)
+#interface used for the internet connection (eth0, wlan0, etc)
 INTERFACE=
 
 #Name of the tunnel interface used to route VPN packets (tun0, tun1, etc)
 TUNNEL=
 
-#Full path of the VPN(.ovpn) file
+#Full path of the VPN(.ovpn) file. Make sure to configure your credentials properly inside this file, so, when the script reconnects, openvpn will not need to ask for your credentials
 VPN_FILE=
--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+#Import config file
 source config.sh
+
+#Get the VPN IP from the VPN file
+IP=$(cat $VPN_FILE | grep "remote " | awk '{print $2}')
+
 trap ctrl_c INT
 
 #Check root permission
@@ -26,12 +32,17 @@ iptables -A INPUT -i $TUNNEL -j ACCEPT
 iptables -A OUTPUT -o $INTERFACE -j DROP
 iptables -A INPUT -i $INTERFACE -j DROP
 
-#Check every 5 seconds if VPN goes down, then reconnect it
+#Check every 20 seconds if VPN goes down, then reconnect it
 while [ true ]
 do
 	if [[ $(nmcli connection) != *$TUNNEL* ]]; then
-		openvpn $VPN_FILE
+		#Kill older openvpn processes to avoid creating new tunnels
+		kill `ps -ef | grep $VPN_FILE | grep -v "grep" | awk '{print $2}'`
+		#Start openvpn
+		openvpn $VPN_FILE &
+	else
+		echo "connected"
 	fi
-	sleep 5
+	sleep 20
 done
 
